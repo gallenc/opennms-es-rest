b4d10206e6f4852d581932c5066634775b6c498a
cgallen <cgallen@opennms.org>
2016-08-09 20:48:57 +0100

    Template initialiser now added. This makes sure templates are loaded in ES before events are sent

--- main-module/src/main/java/org/opennms/plugins/elasticsearch/rest/EventForwarderQueueImpl.java
+++ main-module/src/main/java/org/opennms/plugins/elasticsearch/rest/EventForwarderQueueImpl.java
@@ -1,6 +1,5 @@
 package org.opennms.plugins.elasticsearch.rest;
 
-
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -11,25 +10,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * queues events received from OpenNMS for forwarding to elastic search.
- * drops events if ES not keeping up and queue has become full.  
+ * queues events received from OpenNMS for forwarding to elastic search. drops
+ * events if ES not keeping up and queue has become full.
+ * 
  * @author admin
  *
  */
 public class EventForwarderQueueImpl implements EventForwarder {
 
-	private static final Logger LOG = LoggerFactory.getLogger(EventForwarderQueueImpl.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(EventForwarderQueueImpl.class);
 
-	private Integer maxQueueLength=1000;
-	
+	private Integer maxQueueLength = 1000;
+
 	private LinkedBlockingQueue<Event> queue = null;
 	private AtomicBoolean clientRunning = new AtomicBoolean(false);
 
 	private RemovingConsumer removingConsumer = new RemovingConsumer();
 	private Thread removingConsumerThread = new Thread(removingConsumer);
-	
-	private EventToIndex eventToIndex=null;
-	
+
+	private EventToIndex eventToIndex = null;
+
+	private ElasticSearchInitialiser elasticSearchInitialiser = null;
+
 	public EventToIndex getEventToIndex() {
 		return eventToIndex;
 	}
@@ -37,7 +40,7 @@ public class EventForwarderQueueImpl implements EventForwarder {
 	public void setEventToIndex(EventToIndex eventToIndex) {
 		this.eventToIndex = eventToIndex;
 	}
-	
+
 	public Integer getMaxQueueLength() {
 		return maxQueueLength;
 	}
@@ -45,25 +48,43 @@ public class EventForwarderQueueImpl implements EventForwarder {
 	public void setMaxQueueLength(Integer maxQueueLength) {
 		this.maxQueueLength = maxQueueLength;
 	}
-	
+
 	@Override
-	public void sendNow(Event event) {		
-		if (LOG.isDebugEnabled()) LOG.debug("Event received: queue.size() "+queue.size()
-				+ " queue.remainingCapacity() "+ queue.remainingCapacity()
-				+ "\n   event:" + event.toString());
-		if (! queue.offer(event)){
-			LOG.warn("Cannot queue any more events. Event queue full. size="+queue.size());
-		};
+	public void sendNow(Event event) {
+
+		// if no elasticSearchInitialiser defined then just run
+		// else check if initialised then send event
+		if (elasticSearchInitialiser != null
+				&& elasticSearchInitialiser.isInitialised()) {
+
+			if (LOG.isDebugEnabled())
+				LOG.debug("Event received: queue.size() " + queue.size()
+						+ " queue.remainingCapacity() "
+						+ queue.remainingCapacity() + "\n   event:"
+						+ event.toString());
+			
+			if (!queue.offer(event)) {
+				LOG.warn("ElasticSearch interface discarding event dbid="
+						+ event.getDbid()
+						+ " Cannot queue any more events. Event queue full. size="
+						+ queue.size());
+			}
+		} else {
+			if (LOG.isDebugEnabled())
+				LOG.debug("Not sending event received elasticsearch not initialised"
+						+ "\n   event:" + event.toString());
+		}
 	}
 
 	@Override
 	public void sendNow(Log arg0) {
 		// NOT USED
 	}
-	
-	public void init(){
-		LOG.debug("initialising EventFowarderQueue with queue size "+maxQueueLength);
-		queue =  new LinkedBlockingQueue<Event>(maxQueueLength);
+
+	public void init() {
+		LOG.debug("initialising EventFowarderQueue with queue size "
+				+ maxQueueLength);
+		queue = new LinkedBlockingQueue<Event>(maxQueueLength);
 
 		// start consuming thread
 		clientRunning.set(true);
@@ -71,38 +92,60 @@ public class EventForwarderQueueImpl implements EventForwarder {
 
 	}
 
-	public void destroy(){
+	public void destroy() {
 		LOG.debug("shutting down EventFowarderQueue");
 
 		// signal consuming thread to stop
 		clientRunning.set(false);
 		removingConsumerThread.interrupt();
 	}
-	
+
+	/**
+	 * @return the elasticSearchInitialiser
+	 */
+	public ElasticSearchInitialiser getElasticSearchInitialiser() {
+		return elasticSearchInitialiser;
+	}
+
+	/**
+	 * @param elasticSearchInitialiser
+	 *            the elasticSearchInitialiser to set
+	 */
+	public void setElasticSearchInitialiser(
+			ElasticSearchInitialiser elasticSearchInitialiser) {
+		this.elasticSearchInitialiser = elasticSearchInitialiser;
+	}
+
 	/*
-	 * Class run in separate thread to remove and process notifications from the queue 
+	 * Class run in separate thread to remove and process notifications from the
+	 * queue
 	 */
 	private class RemovingConsumer implements Runnable {
-		//TODO remove final Logger LOG = LoggerFactory.getLogger(EventForwarderQueueImpl.class);
+		// TODO remove final Logger LOG =
+		// LoggerFactory.getLogger(EventForwarderQueueImpl.class);
 
 		@Override
 		public void run() {
 
-			// we remove elements from the queue until interrupted and clientRunning==false.
+			// we remove elements from the queue until interrupted and
+			// clientRunning==false.
 			while (clientRunning.get()) {
 				try {
 					Event event = queue.take();
 
-					if(LOG.isDebugEnabled()) LOG.debug("Event received from queue by consumer thread :\n event:"+event.toString());
-					
+					if (LOG.isDebugEnabled())
+						LOG.debug("Event received from queue by consumer thread :\n event:"
+								+ event.toString());
+
 					// send event to index processor
-					if (eventToIndex!=null){
+					if (eventToIndex != null) {
 						eventToIndex.forwardEvent(event);
-					}else {
+					} else {
 						LOG.error("cannot send event eventToIndex is null");
 					}
 
-				} catch (InterruptedException e) { }
+				} catch (InterruptedException e) {
+				}
 
 			}
 
